c348e443ee036150ef34f6f6ec7e65b372b20419
Jeff Sacks <jrsacks@gmail.com>
2011-02-26 14:53:11 -0600

    adding exception catching and auto refreshing

--- fantasyMarch/app/models/game.rb
+++ fantasyMarch/app/models/game.rb
@@ -12,25 +12,29 @@ class Game
     }
 
     teamScore = Hash.new(0)
-    doc = Hpricot(html)  
-    doc.search("tr[@class=ysprow]").each {|p|
-      idMatch = p.search('a').to_html.match(/[0-9]+/)
-      line = p.inner_html.split("<\/td>")      
-      pointMatch = line[-2].match(/[0-9]+/)
-      unless pointMatch.nil? || idMatch.nil?
-        score = Score.find_or_create_by_playerId_and_gameId(:playerId => idMatch[0], :gameId => gameId)
-        score.points = pointMatch[0]
-        score.save
-        player = Player.where(:playerId => idMatch[0]).first
-        teamScore[player.team] += pointMatch[0].to_i
-      end
-    }
-    if final
-      Rails.logger.info teamScore.index(teamScore.values.min)
-      Player.where(:team => teamScore.index(teamScore.values.min)).each { |p|
-        p.alive = false
-        p.save
+    begin
+      doc = Hpricot(html)  
+      doc.search("tr[@class=ysprow]").each {|p|
+        idMatch = p.search('a').to_html.match(/[0-9]+/)
+        line = p.inner_html.split("<\/td>")      
+        pointMatch = line[-2].match(/[0-9]+/)
+        unless pointMatch.nil? || idMatch.nil?
+          score = Score.find_or_create_by_playerId_and_gameId(:playerId => idMatch[0], :gameId => gameId)
+          score.points = pointMatch[0]
+          score.save
+          player = Player.where(:playerId => idMatch[0]).first
+          teamScore[player.team] += pointMatch[0].to_i
+        end
       }
+      Rails.logger.info teamScore
+      if final
+        Player.where(:team => teamScore.index(teamScore.values.min)).each { |p|
+          p.alive = false
+          p.save
+        }
+      end
+    rescue => e
+      Rails.logger.error "Caught Error: #{e}"
     end
   end
 
