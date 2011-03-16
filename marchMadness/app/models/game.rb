7ad5b2957c34f335f65b0bdd06306247d4e892c8
Jeff Sacks <jrsacks@gmail.com>
2011-03-15 19:12:32 -0500

    fix for utf gameId problem

--- marchMadness/app/models/game.rb
+++ marchMadness/app/models/game.rb
@@ -19,11 +19,11 @@ class Game
       idMatch = p.search('a').to_html.match(/[0-9]+/)
       points = p.search('td').last.text
       unless points.nil? || idMatch.nil?
-        score = Score.find(:first, :conditions => {:playerId => idMatch[0], :gameId => gameId})
+        score = Score.find(:first, :conditions => {:playerId => idMatch[0], :gameId => gameId.to_i - 201100000000})
         if score.nil?
           score = Score.new
           score.playerId = idMatch[0]
-          score.gameId = gameId
+          score.gameId = gameId.to_i - 201100000000
         end
         score.points = points
         score.save
@@ -34,7 +34,6 @@ class Game
       end
     }
 
-    Rails.logger.info teamScore
     if final
       Player.find(:all, :conditions => {:team => teamScore.index(teamScore.values.min)}).each { |p|
         p.alive = false
